cdd80d37d320e63658716b9283fdadf4cb94dd76
Pedro Henrique Da Fonseca Do Nascimento <pedro.henrique.nas@hotmail.com>
2024-09-06 18:48:03 -0300

    Realizado o comprimento de onda conforme solicitado no slide

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print("1.23 x 10^4 --> 1.23e4\n")
 
 while True:
     print("""1. Entrada de Frequência de Onda
-2. 
+2. Entrada de Comprimento de onda
 3.
 4.
 5.
@@ -34,10 +34,22 @@ while True:
         kNotacao = "{:.4e}".format(k)
         wNotacao = "{:.4e}".format(w)
         
-        print("Comprimento de onda = %sm", comprimentoNotacao)
-        print("K = %s rad/m", kNotacao)
-        print("W = %s rad/s", wNotacao) 
+        print(f"Comprimento de onda = {comprimentoNotacao}m" )
+        print(f"K = {kNotacao} rad/m")
+        print(f"W = {wNotacao} rad/s" ) 
         
+    if(opcao == '2'):
+        comprimento = float(input("Digite o comprimento de onda (m): "))
+    
+        f, k, w = comprimentoInput(comprimento)
+
+        fNotacao = "{:.4e}".format(f)
+        kNotacao = "{:.4e}".format(k)
+        wNotacao = "{:.4e}".format(w)
+
+        print(f"Frequência = {fNotacao} Hz")
+        print(f"K = {kNotacao} rad/m" )
+        print(f"W = {wNotacao} rad/s")    
         
     elif(opcao == '0'):
         print("Saindo...")
