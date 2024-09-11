2d338b6ade87cf185475ac40028cd423589de86b
Jônatas da Silva Gonçalves <jngoncalves721@gmail.com>
2024-09-10 21:53:58 -0300

    adicao do print do readme no codigo

--- main.py
+++ main.py
@@ -7,10 +7,34 @@ Wallace dos Santos Izidoro.
 Pedro Henrique da Fonseca do Nascimento.
 Vinícius do Nascimento Generoso.\n""")
 
-print("Calculadora OEM em Python:\n")
+print("""
+Estudo das ondas eletromagnéticas com programação em linguagem Python. Código em python feito para realizar diferentes tipos de cálculos, 
+relacionados a ondas eletromagnéticas Todos os cálculos são acessados através do menu do algoritmo. Passando parâmetros específicos, são 
+exibidos resultados relacionados a ele:
+
+  - Entrada com Frequência de onda: Comprimento de onda, número de onda e frequência angular da onda.
+  - Entrada de Comprimento de onda: Frequência, número de onda e frequência angular.
+  - Entrada com Número de onda: Comprimento de onda, frequência e frequência angular.
+  - Entrada com Frequência angular: Comprimento de onda, número de onda, frequência.
+  - Entrada feita com Amplitude magnética: Amplitude do campo eletrico, intensidade de onda.
+  - Entrada com Amplitude do campo elétrico: Amplitude do campo magnético, intensidade da onda.
+  - Entrada do Intensidade d euma onda eletromagnética: Amplitude do campo magnético, amplitude do campo elétrico\n""")
+
+
+print("""
+Limitações: Os cálculos só são realizados corretamente, caso o usuário use a unidade de medida solicitada pelo algoritmo Breve explicação
+
+Ondas eletromagnéticas consistem em campos elétricos e magnéticos que oscilam perpendicularmente entre si e à direção de propagação. A frequência
+e o comprimento de onda estão relacionados pela velocidade da luz c, assim como a intensidade da onda é determinada pelas amplitudes dos campos 
+elétrico e magnético. A programação em Python pode automatizar esses cálculos fornecendo uma interface para calcular essas propriedades a partir 
+de valores iniciais, como frequência ou comprimento de onda. Um exemplo comum são as ondas de rádio, luz visível, raios-X, entre outras.
+""")
 
 print("Caso deseje entrar com valores em notação científica, utilize o formato abaixo:")
-print("1.23 x 10^4 --> 1.23e4\n")
+print("1.23 x 10^4  --> 1.23e4")
+print("4.32 x 10^-2 --> 4.32e-2\n")
+
+print("\nCalculadora OEM em Python:\n")
 
 while True:
     print("""1. Entrada de Frequência de Onda (F)
